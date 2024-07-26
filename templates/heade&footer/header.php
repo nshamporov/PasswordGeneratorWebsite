1efc51d94458a860cc5116ac9a0dd45eb2c34f02
Nikita <nshamporovofficial@gmail.com>
2024-07-26 23:53:45 +0700

    first commit

---
+++ templates/heade&footer/header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="En-US">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="A website for Passwords Generating and Storing">
+        <meta name="keywords" content="Generator, Password">
+        <meta name="author" content="Nikita Shamporov">
+        <title>Password Generator | Home</title>
+    </head>
+    <body>
+        <h1>Hello World</h1>
+    </body>
+</html>
